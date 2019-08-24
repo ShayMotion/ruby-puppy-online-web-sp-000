1ecd9241cfd02b4874daca3b50a11a2a3284ae7d
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-08-24 21:57:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,21 @@
-class Dog  #belong to a shelter & an owner
-  attr_accessor :name, :age, :breed, :shelter, :owner
-  @@all = []
-  
-  def initialize(name, age, breed)
-    @name = name
-    @age = age
-    @breed = breed 
-    @@all << self 
-  end 
-  
-  def self.all
-    @@all
-  end 
+class Dog
+
+ 	@@all = []
+	attr_accessor :name
+
+ 	def initialize(name)
+		@name = name
+		@@all << self
+	end
+
+ 	def self.clear_all
+		@@all.clear
+
+ 	end
+
+ 	def self.all
+		@@all.each do |dog|
+			puts dog.name
+		end
+	end
 end 
-    
